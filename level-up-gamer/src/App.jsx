9e6c7bc01ce4619a1a40f2c2dabf05e6423e3726
AranOpazo <ara.opazo@duocuc.cl>
2025-10-11 10:56:21 -0300

    style(ui): restauro paleta, grid y creo App.jsx - Nuevo styles/main.css con variables y layout (.wrapper). - Nuevo App.jsx que centraliza rutas y Header/Footer. - login.css separado para la pantalla de autenticación.

---
+++ level-up-gamer/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Productos from './pages/Productos.jsx';
+import Carrito from './pages/Carrito.jsx';
+import Perfil from './pages/Perfil.jsx';
+
+import "./App.css";  
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/productos" element={<Productos/>}/>
+        <Route path="/carrito" element={<Carrito/>}/>
+        <Route path="/perfil" element={<Perfil/>}/>
+      </Routes>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;

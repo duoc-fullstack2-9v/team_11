b09f2af267303c3a5d9cdf4f97518545be9a7737
AranOpazo <ara.opazo@duocuc.cl>
2025-10-11 10:58:58 -0300

    (core): BrowserRouter en main y bootstrap único - main.jsx envuelve <App /> en <BrowserRouter>. - Bootstrap CSS/JS solo se carga aquí. - package.json/lock: se añade react-router-dom.

--- level-up-gamer/src/main.jsx
+++ level-up-gamer/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App.jsx'
 import './index.css'
-import Home from './Home.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )

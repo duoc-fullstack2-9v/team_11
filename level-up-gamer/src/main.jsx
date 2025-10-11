cf53e570c71d9b9a81348b498088a01f0d5c3b8e
AranOpazo <ara.opazo@duocuc.cl>
2025-10-11 11:39:10 -0300

    styles(imports): incluyo main.jsx en la limpieza de CSS

--- level-up-gamer/src/main.jsx
+++ level-up-gamer/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './styles/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 

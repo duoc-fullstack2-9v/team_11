45c9d1dd43310bec5d17392f55fce6e14b3584e0
IsaOfViltrum <is.sanchezy@duocuc.cl>
2025-10-11 22:10:46 -0300

    hook useParams

--- level-up-gamer/src/pages/Productos.jsx
+++ level-up-gamer/src/pages/Productos.jsx
@@ -1,102 +1,83 @@
+import { useLocation } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { useParams } from "react-router-dom";
 
-import { useLocation } from 'react-router-dom';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-
+const productos = [
+  {
+    id: "1",
+    titulo: "Street Fighter vs Tekken",
+    imagen: "/imgs/street x tekken.webp",
+    precio: 29990,
+  },
+  {
+    id: "2",
+    titulo: "Elden Ring",
+    imagen: "/imgs/elden-ring-portada.jpg",
+    precio: 29990,
+  },
+  { id: "3", titulo: "Skyrim", imagen: "/imgs/skyrim.webp", precio: 29990 },
+  {
+    id: "4",
+    titulo: "Red Dead Redemption II",
+    imagen: "/imgs/redDead.avif",
+    precio: 29990,
+  },
+  {
+    id: "5",
+    titulo: "Silent Hill F",
+    imagen: "/imgs/silenciogill.png",
+    precio: 29990,
+  },
+  {
+    id: "6",
+    titulo: "Resident Evil 4",
+    imagen: "/imgs/resident-evil-4-hd-proyect-generacionxbox.jpg",
+    precio: 29990,
+  },
+  {
+    id: "7",
+    titulo: "Resident Evil 7",
+    imagen: "/imgs/resident7.avif",
+    precio: 29990,
+  },
+  {
+    id: "8",
+    titulo: "Bioshock",
+    imagen: "/imgs/BioShock_cover.jpg",
+    precio: 29990,
+  },
+  {
+    id: "9",
+    titulo: "Devil My Cry 5",
+    imagen: "/imgs/devilmycry.jpg",
+    precio: 29990,
+  },
+  { id: "10", titulo: "Bayonetta", imagen: "/imgs/lamamu.jpg", precio: 29990 },
+];
 function Productos() {
-    const location=useLocation();
-    console.log(location);
-    
-    return (
+  const location = useLocation();
+  console.log(location);
+  const { id } = useParams();
+
+  return (
     <>
-        
-            <main>
-                <h2 className="titulo-principal">Compra los mejores productos al mejor precio!</h2>
-                <div className="contenedor-producto">
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/street x tekken.webp" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Street Figther vs Tekken</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/elden-ring-portada.jpg" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Elden Ring</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/skyrim.webp" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Skyrim </h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/redDead.avif" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Red dead redemption ll</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/silenciogill.png" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Sillent hill F</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/resident-evil-4-hd-proyect-generacionxbox.jpg" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Resident evil 4</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/resident7.avif" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Resident evil 7</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/BioShock_cover.jpg" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Bioshock </h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/devilmycry.jpg" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Devil my cry 5</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                    <div className="producto">
-                        <img className="producto-imagen" src="./imgs/lamamu.jpg" alt=""/>
-                        <div className="producto-detalles">
-                            <h3 className="producto-titulo">Bayonetta'</h3>
-                            <p className="producto-precio">$29.990</p>
-                            <button className="producto-agregar">Agregar</button>
-                        </div>
-                    </div>
-                </div>
-            </main>
+      <main>
+        <h2 className="titulo-principal">
+          Compra los mejores productos al mejor precio!
+        </h2>
+        <div className="contenedor-producto">
+          {productos.map((p) => (
+            <ProductoHome
+              key={p.id}
+              producto={p}
+              onAgregarClick={handleAgregar}
+            />
+          ))}
+        </div>
+      </main>
     </>
-    )
+  );
 }
 
 export default Productos;

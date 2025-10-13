0d0175a78af8a2ad2b480c687295957c92bc4aaf
AranOpazo <ara.opazo@duocuc.cl>
2025-10-13 11:31:31 -0300

    Modificación de import mal escrito.

--- level-up-gamer/src/pages/Login.jsx
+++ level-up-gamer/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { findUser, saveUser, startSession } from "../utils/auth";
-import "../styles/inicioSesion.css";
+import "../styles/Login.css";
 
 function InicioSesion() {
     const [view, setView] = useState("inicioSesion");

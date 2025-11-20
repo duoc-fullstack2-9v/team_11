3aa949fa8c47d889a88344972b6920a8e2b93394
AranOpazo <ara.opazo@duocuc.cl>
2025-11-20 00:57:10 -0300

    Se hace crud

---
+++ frontend/src/services/productoService.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/productos/"; // REEMPLAZAR CON LA URL DE NUESTRA API
+
+// Listar todos los productos
+export const listarProductos = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (e) {
+        console.error("Error al listar productos: ", e);
+        throw e;
+    }
+};
+
+// Obtener un producto por Id
+export const obtenerProductoPorId = async (id) => {
+    try {
+        const response = await axios.get(API_URL + id);
+        return response.data;        
+    } catch (e) {
+        console.error("Erros al obtener producto por id: ", e);
+        throw e;
+    }
+};
+
+// Crear un producto
+export const crearProducto = async (producto) => {
+    try {
+        const response = await axios.post(API_URL, producto);
+        return response.data;
+    } catch (e) {
+        console.error("Error al crear el producto: ", e);
+        throw e;        
+    }
+};
+
+// Axtualizar producto
+export const actualizarProducto = async (id, producto) => {
+    try {
+        const response = await axios.put(API_URL + id, producto);
+        return response.data;    
+    } catch (e) {
+        console.error("Error al actualizar el producto con id " + id, ": ", e);
+        throw e;        
+    }
+};
+
+// Eliminar un producto
+export const eliminarProducto = async (id) => {
+    try {
+        const response = await axios.delete(API_URL + id);
+        return response.data;
+    } catch (e) {
+        console.error("Error al eliminar el producto con id " + id, ": ", e);
+        throw e;        
+    }
+}

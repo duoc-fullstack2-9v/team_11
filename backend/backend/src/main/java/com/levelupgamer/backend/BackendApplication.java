7af9bd89dc5dc0e60a7a393e43a8d2fb412fd251
IsaOfViltrum <is.sanchezy@duocuc.cl>
2025-11-30 23:13:17 -0300

    arreglo de backend

--- backend/backend/src/main/java/com/levelupgamer/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/levelupgamer/backend/BackendApplication.java
@@ -1,14 +1,14 @@
 package com.levelupgamer.backend;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.levelupgamer.backend") // <-- Agrega ESTA LÍNEA
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
--- backend/backend/src/main/java/com/levelupgamer/backend/repository/ProductoRepository.java
+++ backend/backend/src/main/java/com/levelupgamer/backend/repository/ProductoRepository.java
@@ -2,6 +2,8 @@ package com.levelupgamer.backend.repository;
 
 import com.levelupgamer.backend.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, String> {
 }

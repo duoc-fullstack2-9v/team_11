6facfd36d6cf0f85df26b971bfaa5bb91e8b62bf
AranOpazo <ara.opazo@duocuc.cl>
2025-12-07 19:35:01 -0300

    Se agrega url de EC2 correcta.

--- frontend/src/services/productoService.jsx
+++ frontend/src/services/productoService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//const API_URL = "https://EC2-AQUI/productos"; // REEMPLAZAR CON LA URL DE NUESTRA API
+//const API_URL = "http://ec2-3-212-50-160.compute-1.amazonaws.com/productos"; // REEMPLAZAR CON LA URL DE NUESTRA API
 const API_URL = "http://localhost:8080/productos"; // PARA PROBAR LOCALMENTE
 
 const urlConId = (id) => `${API_URL}/${id}`;

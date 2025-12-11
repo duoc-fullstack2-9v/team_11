39b855cacc88725c9101eba97d590ac83e25fcec
AranOpazo <ara.opazo@duocuc.cl>
2025-12-11 02:22:48 -0300

    Se agrega botón de cerrar sesión si hay sesión iniciada

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useCarrito } from "../context/CarritoContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { getSession, endSession } from "../utils/auth";
 
 
 import logo from "../assets/imgs/imagen_2025-09-05_021420718-removebg-preview.png";
@@ -8,12 +10,21 @@ function Header() {
   // Hook del carrito
   const { carrito } = useCarrito();
 
+  // Hook de navegación
+  const navigate = useNavigate();
+  const sesion = getSession(); // null si no hay usuario
+
+  const cerrarSesion = () => {
+    endSession();
+    navigate("/login");
+  };
+
   // Calcular total de ítems
   const totalItems = carrito.reduce((acc, producto) => acc + producto.cantidad, 0);
 
   return (
     <aside>
-      
+
       <img className="logo-imagen" src={logo} alt="Logo de mi tienda" />
 
       <header>
@@ -23,11 +34,38 @@ function Header() {
       <nav>
         <ul className="menu">
           {/* Iniciar sesión */}
-          <li>
+          {/* <li>
             <NavLink className="boton-menu boton login" to="/login">
               <i className="bi bi-person-raised-hand"></i> Iniciar sesión
             </NavLink>
-          </li>
+          </li> */}
+
+          {/* Iniciar sesión / Cerrar sesión */}
+          {!sesion ? (
+            // Mostrar "Iniciar sesión" si NO hay sesión
+            <li>
+              <NavLink className="boton-menu boton login" to="/login">
+                <i className="bi bi-person-raised-hand"></i> Iniciar sesión
+              </NavLink>
+            </li>
+          ) : (
+            // Mostrar "Cerrar sesión" si SÍ hay sesión
+            <li>
+              <button
+                className="boton-menu boton login"
+                onClick={cerrarSesion}
+                style={{
+                  border: "none",
+                  background: "none",
+                  cursor: "pointer",
+                  width: "100%",
+                  textAlign: "left"
+                }}
+              >
+                <i className="bi bi-box-arrow-right"></i> Cerrar sesión
+              </button>
+            </li>
+          )}
 
           {/* Home */}
           <li>
